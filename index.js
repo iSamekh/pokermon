709abc68917be88b8bd6cb76fee213c47c589761
Eric Gomes <ee-gomes@outlook.com>
2023-04-08 00:45:00 -0300

    Feat Login and Register

---
+++ index.js
@@ -0,0 +1,15 @@
+if (localStorage.getItem("token") == null) {
+    alert("Faça o login");
+    window.location.href = "../Login/login.html";
+}
+
+const userOnline = JSON.parse(localStorage.getItem("userOnline"));
+
+const user = document.querySelector("#user");
+user.innerHTML = `Hello ${userOnline.name}`;
+
+function quit() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userOnline");
+    window.location.href = "../Login/login.html";
+}
